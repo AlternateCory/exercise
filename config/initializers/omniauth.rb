78493492e1f5bfc44da9ca12f255fbe871cd0a1e
Cory L. Rooker <crooker@uncc.edu>
2018-04-20 09:18:27 -0400

    Install and Set Up Cucumber

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 OmniAuth.config.logger = Rails.logger
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '915949595432-4f2ku7i0tjb1ispdoieejatpnfv7t913.apps.googleusercontent.com', 'NDerDhihauRHQ6NLdk3dj24b', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '915949595432-te9msaqmmmjjgrvgf4n74qrq4ft8aoid.apps.googleusercontent.com', 'hQAf_k1pbl-AuX_kb0thU2Hr', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180418170605) do
+ActiveRecord::Schema.define(version: 20180418172306) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 20180418170605) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["workout_id"], name: "index_premades_on_workout_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "workouts", force: :cascade do |t|
